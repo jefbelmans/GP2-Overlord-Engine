06a68c0dff71d041443e4da00c5b69e759fb07ab
Jef Belmans <belmansjef@gmail.com>
2023-05-08 17:46:15 +0200

    Started on ParticleEmitter

--- OverlordEngine/Components/ParticleEmitterComponent.cpp
+++ OverlordEngine/Components/ParticleEmitterComponent.cpp
@@ -16,22 +16,79 @@ ParticleEmitterComponent::ParticleEmitterComponent(const std::wstring& assetFile
 
 ParticleEmitterComponent::~ParticleEmitterComponent()
 {
-	TODO_W9(L"Implement Destructor")
+	delete[] m_ParticlesArray;
+	m_pVertexBuffer->Release();
 }
 
-void ParticleEmitterComponent::Initialize(const SceneContext& /*sceneContext*/)
+void ParticleEmitterComponent::Initialize(const SceneContext& sceneContext)
 {
-	TODO_W9(L"Implement Initialize")
+	if (m_pParticleMaterial == nullptr)
+		MaterialManager::Get()->CreateMaterial<ParticleMaterial>();
+
+	CreateVertexBuffer(sceneContext);
+	m_pParticleTexture = ContentManager::Load<TextureData>(m_AssetFile);
 }
 
-void ParticleEmitterComponent::CreateVertexBuffer(const SceneContext& /*sceneContext*/)
+void ParticleEmitterComponent::CreateVertexBuffer(const SceneContext& sceneContext)
 {
-	TODO_W9(L"Implement CreateVertexBuffer")
+	if(m_pVertexBuffer)
+		m_pVertexBuffer->Release();
+
+	//a.Set the usage to Dynamic
+	D3D11_BUFFER_DESC bufferDesc{};
+	bufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+
+	//b.Use the ParticleCount and size of a VertexParticle to calculate the ByteWidth
+	bufferDesc.ByteWidth = m_ParticleCount * sizeof(VertexParticle);
+
+	//c.This is a VertexBuffer, select the appropriate BindFlag
+	bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+
+	//d.We want to edit the buffer at - runtime, so we need CPU write access.Select the
+	//appropriate CPUAccessFlag
+	bufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+	//e.There are no MiscFlags(0)
+	bufferDesc.MiscFlags = 0;
+
+	//f.Create the VertexBuffer(GraphicsDevice > …)
+	HRESULT hr = sceneContext.d3dContext.pDevice->CreateBuffer(&bufferDesc, nullptr, &m_pVertexBuffer);
+
+	//g.Don’t forget to check for errors!(HANDLE_ERROR)
+	if (FAILED(hr))
+		Logger::LogError(L"ParticleEmitterComponent::CreateVertexBuffer() > Failed to create vertex buffer!");
 }
 
 void ParticleEmitterComponent::Update(const SceneContext& /*sceneContext*/)
 {
-	TODO_W9(L"Implement Update")
+	/*float particleInterval{ (m_EmitterSettings.maxEnergy - m_EmitterSettings.minEnergy) / m_ParticleCount };
+	m_LastParticleSpawn += sceneContext.pGameTime->GetElapsed();
+
+	m_ActiveParticles = 0;
+
+	D3D11_MAPPED_SUBRESOURCE mappedResource{};
+	HRESULT hr = sceneContext.d3dContext.pDeviceContext->Map(m_pVertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+
+	if (FAILED(hr))
+		Logger::LogError(L"ParticleEmitterComponent::Update() > Failed to map vertex buffer!");
+
+	VertexParticle* pVertexParticle = static_cast<VertexParticle*>(mappedResource.pData);
+
+	const float dt = sceneContext.pGameTime->GetElapsed();
+	for (size_t i = 0; i < m_ParticleCount; i++)
+	{
+		if (m_ParticlesArray[i].isActive)
+		{
+			UpdateParticle(m_ParticlesArray[i], dt);
+			return;
+		}
+			
+		
+		if (!m_ParticlesArray[i].isActive && m_LastParticleSpawn >= particleInterval)
+		{
+
+		}
+	}*/
 }
 
 void ParticleEmitterComponent::UpdateParticle(Particle& /*p*/, float /*elapsedTime*/) const
--- OverlordProject/MainGame.cpp
+++ OverlordProject/MainGame.cpp
@@ -7,7 +7,7 @@
 // #define W5
 // #define W6
 // #define W7
-//#define W8
+// #define W8
 // #define W9
 // #define W10
 
